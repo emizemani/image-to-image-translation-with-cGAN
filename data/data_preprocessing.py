39e3ed0fb9653babd7e1d6f59c6a84bafd693b72
Felix <fritz.apple@gmail.com>
2024-11-12 18:36:01 +0100

    fixed typo

--- data/data_preprocessing.py
+++ data/data_preprocessing.py
@@ -14,8 +14,8 @@ warnings.filterwarnings("ignore")
 #plt.ion()   # interactive mode: The interactive mode is mainly useful if you build plots from the command line and want to see the effect of each command while you are building the figure.
 
 def add_root(search_filename):
-    '''Add a root to the XML-file to be able to use the XML parser
-       The whole folder of the dataset called "Base dataset" from https://cmp.felk.cvut.cz/~tylecr1/facade/ has bo be located in the same folder as the code project 
+    '''Add a root structure <root> ... </root> to the XML-file to be able to use the XML parser
+       The dataset called "Base dataset" from https://cmp.felk.cvut.cz/~tylecr1/facade/ is originally missing the root 
     '''
     # Get the list of all files in the current directory
     files_in_directory = os.listdir('./data/CMP_facade_DB_base/base/')
@@ -23,12 +23,11 @@ def add_root(search_filename):
     # Filter only .xml files 
     xml_files = [f for f in files_in_directory if f.endswith('.xml')]
 
-    # Check if the search_filename is present in any of the .xml filenames
-    matching_files = [f for f in xml_files if search_filename not in f]
+    # Check if the search_filename is already present in any of the .xml filenames
+    initial_files = [f for f in xml_files if search_filename not in f]
     
-    if matching_files:
-        for file in matching_files:
-            #print(file)
+    if initial_files:
+        for file in initial_files:
             #Add a root to the XML-file to be able to use the XML parser
             with open(os.path.join('./data/CMP_facade_DB_base/base//{}'.format(file)), 'r') as f, open(os.path.join('./data/CMP_facade_DB_base/base/{}'.format(file)), 'w') as g:
                 g.write('<root>{}</root>'.format(f.read()))
@@ -39,7 +38,7 @@ def add_root(search_filename):
 add_root("root")  
 
 
-'''
+
 #https://docs.python.org/3/library/xml.etree.elementtree.html#xml.etree.ElementTree.TreeBuilder.data
 import xml.etree.ElementTree as ET
 tree = ET.parse('/Users/felix/Desktop/PML/PML code/CMP_facade_DB_base/base/cmp_b0001root.xml')
@@ -52,4 +51,3 @@ for child in root.findall('object'):
 print(root[0][0].text)
 print(root[0][1].text)
 print ("XXXXXXXXXXXXXXXX")
-'''
