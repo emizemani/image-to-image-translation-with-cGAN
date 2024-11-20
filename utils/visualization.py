0ee58f94ba04d66ff6b196ec831bf056fb48bb8a
Felix <fritz.apple@gmail.com>
2024-11-20 21:11:26 +0100

    made visualization usable to show every dataset

--- utils/visualization.py
+++ utils/visualization.py
@@ -7,21 +7,14 @@ import torch
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
 
-
-def show(image_label_pair=None, outfile=None):
-
-    config = load_config()
-
-    train_dataset = CustomDataset(
-        images_dir=config['data']['train_images_dir'],
-        labels_dir=config['data']['train_labels_dir'])
+def show(image_label_pair=None, outfile=None, dataset=None):
     #Debug
     #print(len(train_dataset))
     
     batch=[]
     for pair in image_label_pair:
-        batch.append(train_dataset[pair]["A"])
-        batch.append(train_dataset[pair]["B"])
+        batch.append(dataset[pair]["A"])
+        batch.append(dataset[pair]["B"])
 
     # Create a grid of images
     grid = make_grid(batch, nrow=2) 
@@ -37,9 +30,15 @@ def show(image_label_pair=None, outfile=None):
         plt.show()
     else:
         plt.savefig('images_to_show.png')
-        
+
     return 
 
+# Get images and associated labels from training dataset
+config = load_config()
+show_dataset = CustomDataset(
+    images_dir=config['data']['train_images_dir'],
+    labels_dir=config['data']['train_labels_dir'])
+
 # Show pairs of images and associated labels
-images_to_show = [1,2,3,10,23]
-show(images_to_show)
+images_to_show = [1,3,10,23]
+show(images_to_show,None,show_dataset)
