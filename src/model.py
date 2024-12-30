abb41eb7652a7fca53b3a56732df1e341f575df9
benedikt <b.kunz@tu-berlin.de>
2024-12-30 10:08:40 +0100

    set config for next training

--- src/model.py
+++ src/model.py
@@ -10,8 +10,8 @@ class UNetGenerator(nn.Module):
         
         # Encoder layers
         self.encoder = nn.ModuleList([
-            self.downsample(in_channels, features, dropout_rate=0),
-            self.downsample(features, features * 2, dropout_rate=0),
+            self.downsample(in_channels, features),
+            self.downsample(features, features * 2),
             self.downsample(features * 2, features * 4),
             self.downsample(features * 4, features * 8),
         ])
@@ -20,7 +20,7 @@ class UNetGenerator(nn.Module):
         self.decoder = nn.ModuleList([
             self.upsample(features * 8, features * 4),
             self.upsample(features * 4 * 2, features * 2),  # Double input channels for concatenated skip connections
-            self.upsample(features * 2 * 2, features, dropout_rate=0),       # Double input channels for concatenated skip connections
+            self.upsample(features * 2 * 2, features),       # Double input channels for concatenated skip connections
         ])
 
         # Final layer for output
@@ -41,7 +41,7 @@ class UNetGenerator(nn.Module):
             nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
             nn.InstanceNorm2d(out_channels),
             nn.ReLU(inplace=False),
-            nn.Dropout2d(dropout_rate)
+            # nn.Dropout2d(dropout_rate)
         )
         
     def forward(self, x):
