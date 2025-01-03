1f57c1a14fc575211c46f38cc81eef58da5a1e00
benedikt <b.kunz@tu-berlin.de>
2025-01-03 21:25:23 +0100

    changes script

--- SLURM_job_script.sh
+++ SLURM_job_script.sh
@@ -1,24 +1,15 @@
-#!/bin/bash
-#SBATCH --partition=gpu                # Request the GPU partition
-#SBATCH --gres=gpu:1                   # Request 1 GPU
-#SBATCH --ntasks=1                     # Number of tasks (usually 1 for PyTorch)
-#SBATCH --cpus-per-task=4              # Number of CPU cores per task
-#SBATCH --mem=16G                      # Memory per node
-#SBATCH --time=04:00:00                # Maximum runtime (hh:mm:ss)
-#SBATCH --job-name=image_translation   # Job name
-#SBATCH --output=logs/output_%j.log    # Save the output to a log file (unique ID: %j)
-#SBATCH --error=logs/error_%j.log      # Save errors to a log file (unique ID: %j)
+#!/bin/bash -l
 
-# Load necessary modules (adjust to your cluster's setup)
-# module load python/3.10                # 
-# module load cuda/11.8                  # CUDA version (we need to ensure compatibility)
+#SBATCH --job-name=image_translation
+#SBATCH --partition=gpu-teaching-2h # Run on the 2h GPU runtime partition, also 5h, 2d and 7d availabl
+#SBATCH --gpus-per-node=40gb:1 # One A100 with 40GB
+#SBATCH --ntasks-per-node=4 # 4 CPU threads
+#SBATCH --output=/home/pml12/script/output.txt
+#SBATCH --error=/home/pml12/script/error.txt
+#SBATCH --chdir=/home/pml12
+#SBATCH --array=1 # run one times
 
-# Maybe activate Conda environment?
-source activate image_translation               
+container=/MS2/image-to-image-translation-with-cGAN/pml.sif
 
-# Navigate to the project directory
-cd image-to-image-translation-with-cGAN  
-
-
-# Run the script
-python scripts/train_apply.py
+apptainer run --nv $container \
+    python src/train_apply.py
