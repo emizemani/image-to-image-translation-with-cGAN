649b86b4ef235d0942037063b0d1419414c5149e
pml12 <pml12@hydra.ml.tu-berlin.de>
2025-01-26 15:07:33 +0100

    added test results

--- SLURM_job_script.sh
+++ SLURM_job_script.sh
@@ -3,9 +3,8 @@
 #SBATCH --job-name=image_translation
 #SBATCH --partition=gpu-teaching-2d # Run on the 2h GPU runtime partition, also 5h, 2d and 7d available
 #SBATCH --gpus=1
-current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
-#SBATCH --output=/home/pml12/script/r-${current_datetime}/output.txt
-#SBATCH --error=/home/pml12/script/r-${current_datetime}/error.txt
+#SBATCH --output=/home/pml12/script/r-%j/output.txt
+#SBATCH --error=/home/pml12/script/r-%j/error.txt
 #SBATCH --chdir=/home/pml12/MS2/image-to-image-translation-with-cGAN
 
 container=pml.sif
