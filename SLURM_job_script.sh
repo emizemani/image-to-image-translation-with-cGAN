56f2b1750fcd8f031920f32506c08063e08a48ff
benedikt <b.kunz@tu-berlin.de>
2025-01-31 23:25:56 +0100

    last changes

--- SLURM_job_script.sh
+++ SLURM_job_script.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=image_translation
 #SBATCH --partition=gpu-teaching-2d # Run on the 2h GPU runtime partition, also 5h, 2d and 7d available
-#SBATCH --gpus=8
+#SBATCH --gpus=1
 #SBATCH --output=/home/pml12/script/r-%j/output.txt
 #SBATCH --error=/home/pml12/script/r-%j/error.txt
 #SBATCH --chdir=/home/pml12/MS2/image-to-image-translation-with-cGAN
 
 container=pml.sif
 
-apptainer run --nv $container python -u utils/analyze_discriminator.py
+apptainer run --nv $container python -u src/train_apply.py
